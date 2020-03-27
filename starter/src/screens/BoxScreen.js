21b1ee74df62231abf88482e7cf50b702fed70be
Mario Lazzari <mario.lazzari@gmail.com>
2020-03-27 22:42:04 +0100

    Done chapter 7

--- starter/src/screens/BoxScreen.js
+++ starter/src/screens/BoxScreen.js
@@ -3,10 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.text1Style}>Child number 1</Text>
-      <Text style={styles.text2Style}>Child number 2</Text>
-      <Text style={styles.text3Style}>Child number 3</Text>
+    <View>
+      <View style={styles.viewStyle}>
+        <Text style={styles.text1Style}>Child number 1</Text>
+        <Text style={styles.text2Style}>Child number 2</Text>
+        <Text style={styles.text3Style}>Child number 3</Text>
+      </View>
+
+      <View style={styles.containerStyle}>
+        <View style={styles.redStyle}></View>
+        <View style={styles.greenStyle}></View>
+        <View style={styles.purpleStyle}></View>
+      </View>
     </View>
   );
 };
@@ -27,10 +35,29 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: "flex-end",
     borderWidth: 3,
-    borderColor: "red"
+    borderColor: "red",
+    ...StyleSheet.absoluteFillObject
   },
   text3WStyle: {
     borderWidth: 3,
     borderColor: "red"
-  }
+  },
+  containerStyle: {
+    flexDirection: "row",
+    height: 200,
+    borderWidth: 3,
+    justifyContent: "space-between"
+  },
+  redStyle: {
+    backgroundColor: "red",
+    height: 100,
+    width: 100
+  },
+  greenStyle: {
+    backgroundColor: "green",
+    height: 100,
+    width: 100,
+    alignSelf: "flex-end"
+  },
+  purpleStyle: { backgroundColor: "purple", height: 100, width: 100 }
 });
